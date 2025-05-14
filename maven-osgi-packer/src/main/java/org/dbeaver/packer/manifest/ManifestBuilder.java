6ce6da81ab65fa03ccf0f59a9ae26d19cbe266b0
Georgii Gvinepadze <fireshowel@gmail.com>
2025-05-14 14:39:58 +0400

    dbeaver/pro#4153 add packer to osgi-common

---
+++ maven-osgi-packer/src/main/java/org/dbeaver/packer/manifest/ManifestBuilder.java
@@ -0,0 +1,188 @@
+package org.dbeaver.packer.manifest;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
+
+public class ManifestBuilder {
+    public static String buildManifest(
+        String symbolicName,
+        String moduleName,
+        String moduleVersion,
+        List<String> classpaths,
+        Path basedir,
+        Path fragPath
+    ) {
+
+        StringBuilder manifest = new StringBuilder();
+        manifest.append("Manifest-Version: 1.0\n");
+        manifest.append("Bundle-ManifestVersion: 2\n");
+        manifest.append("Bundle-SymbolicName: ").append(symbolicName).append("\n");
+        manifest.append("Bundle-Version: ").append(adaptVersion(moduleVersion)).append("\n");
+        manifest.append("Bundle-Name: ").append(moduleName).append("\n");
+        manifest.append("Bundle-ActivationPolicy: lazy\n");
+        manifest.append("Bundle-RequiredExecutionEnvironment: JavaSE-17\n");
+        // write classpath
+        manifest.append("Bundle-ClassPath: \n");
+
+        for (String classpath : classpaths) {
+            manifest.append(" ").append(basedir.relativize(Paths.get(classpath)));
+            if (!classpaths.get(classpaths.size() - 1).equals(classpath)) {
+                manifest.append(",\n");
+            }
+
+        }
+        manifest.append("\n");
+        // Real all jar files in classpath, extract packages as exports, if packages have no exports in manifest get all packages
+        final boolean[] hasExportPackage = {false};
+        Set<String> packages = new LinkedHashSet<>();
+        for (String classpath : classpaths) {
+            try (JarFile jarFile = new JarFile(classpath)) {
+                Manifest mf = jarFile.getManifest();
+                AtomicBoolean currentClasspathContainsExportPackage = new AtomicBoolean(false);
+                if (mf != null) {
+                    mf.getMainAttributes().forEach((key, value) -> splitByCommaOutsideQuotes(value.toString()).forEach(v -> {
+                        if (key.toString().startsWith("Export-Package")) {
+                            String pkg = !v.contains(";") ? v : v.split(";")[0].trim();
+                            if (packages.contains(pkg)) {
+                                return;
+                            }
+                            packages.add(pkg);
+                            if (!hasExportPackage[0]) {
+                                currentClasspathContainsExportPackage.set(true);
+                                manifest.append(key).append(": \n ").append(v);
+                                hasExportPackage[0] = true;
+                            } else {
+                                manifest.append(",\n ").append(v);
+                                currentClasspathContainsExportPackage.set(true);
+                            }
+                        }
+                    }));
+                }
+                if (!currentClasspathContainsExportPackage.get()) {
+                    // get all packages from the jar file outside manifest
+                    jarFile.stream().filter(e -> e.getName().endsWith(".class")).filter(e -> !e.getName().contains("META-INF"))
+                        .forEach(e -> {
+                            String className = e.getName();
+                            int lastSlash = className.lastIndexOf('/');
+                            if (lastSlash > 0) {
+                                String pkg = className.substring(0, lastSlash).replace('/', '.');
+                                if (packages.contains(pkg)) {
+                                    return;
+                                }
+                                packages.add(pkg);
+                                if (!hasExportPackage[0]) {
+                                    manifest.append("Export-Package").append(": \n ").append(pkg);
+                                    hasExportPackage[0] = true;
+                                } else {
+                                    manifest.append(",\n ").append(pkg);
+                                }
+                            }
+                        });
+                }
+            } catch (IOException e) {
+                System.out.println("Error reading jar file: " + e.getMessage());
+            }
+        }
+        manifest.append("\n");
+        // dependencies, extract parameters from fragPath .MF file\
+        provideDependencies(fragPath, manifest);
+        manifest.append("\n");
+        return manifest.toString();
+    }
+
+    public static String getDefaultBuildProperties() {
+        return """
+            source.. =
+            bin.includes = META-INF/,\\
+                           lib/
+            src.includes = META-INF/        
+            """;
+    }
+
+    private static void provideDependencies(Path fragPath, StringBuilder manifest) {
+        try (FileInputStream fos = new FileInputStream(fragPath.toFile())) {
+            Manifest mf = new Manifest(fos);
+            mf.getMainAttributes().forEach((key, value) -> {
+                boolean isFirst = true;
+                List<String> strings = splitByCommaOutsideQuotes(value.toString());
+                for (String string : strings) {
+                    if (isFirst) {
+                        manifest.append(key).append(": \n ").append(string);
+                        isFirst = false;
+                    } else {
+                        manifest.append(",\n ").append(string);
+                    }
+                }
+                manifest.append("\n");
+            });
+        } catch (Exception e) {
+            System.out.println("Error extracting dependencies: " + e.getMessage());
+        }
+    }
+
+    private static String adaptVersion(String moduleVersion) {
+        //adapt version to OSGI format
+        String[] parts = moduleVersion.split("\\.");
+        StringBuilder adaptedVersion = new StringBuilder();
+        boolean isFirst = true;
+        for (String part : parts) {
+            if (part.matches("\\d+")) {
+                if (isFirst) {
+                    adaptedVersion.append(part);
+                    isFirst = false;
+                } else {
+                    adaptedVersion.append(".").append(part);
+                }
+            } else {
+                if (isFirst) {
+                    adaptedVersion.append(part.replaceAll("[^a-zA-Z0-9]", "_"));
+                    isFirst = false;
+                } else {
+                    adaptedVersion.append(".").append(part.replaceAll("[^a-zA-Z0-9]", "_"));
+                }
+
+            }
+        }
+        // SNAPSHOT versions are not allowed in OSGI remove them
+        if (adaptedVersion.toString().endsWith("_SNAPSHOT")) {
+            adaptedVersion.delete(adaptedVersion.length() - 9, adaptedVersion.length());
+        }
+        return adaptedVersion.toString();
+    }
+
+    private static List<String> splitByCommaOutsideQuotes(String input) {
+        List<String> result = new ArrayList<>();
+        StringBuilder current = new StringBuilder();
+        boolean insideQuotes = false;
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+
+            if (c == '"') {
+                insideQuotes = !insideQuotes; // Toggle quote mode
+                current.append(c);
+            } else if (c == ',' && !insideQuotes) {
+                result.add(current.toString());
+                current.setLength(0); // Reset buffer
+            } else {
+                current.append(c);
+            }
+        }
+
+        if (!current.isEmpty()) {
+            result.add(current.toString());
+        }
+
+        return result;
+    }
+
+}
---
+++ maven-osgi-packer/src/main/java/org/dbeaver/packer/pom/PomBuilder.java
@@ -0,0 +1,26 @@
+package org.dbeaver.packer.pom;
+
+import java.nio.file.Path;
+
+public class PomBuilder {
+    public static String buildPom(String groupId, String artifactId, String version, Path basedir) {
+        return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+            "<project xmlns=\"http://maven.apache.org/POM/4.0.0\"\n" +
+            "         xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n" +
+            "         xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 " +
+            "http://maven.apache.org/xsd/maven-4.0.0.xsd\">\n" +
+            "    <modelVersion>4.0.0</modelVersion>\n\n" +
+            "    <groupId>" + groupId + "</groupId>\n" +
+            "    <artifactId>" + artifactId + "</artifactId>\n" +
+            "    <packaging>" + "eclipse-plugin" + "</packaging>\n" +
+            "    <version>" + version + "</version>\n" +
+            "    <parent>\n" +
+            "           <groupId>com.dbeaver.osgi</groupId>" + "\n" +
+            "           <artifactId>external</artifactId>" + "\n" +
+            "           <version>1.0.0-SNAPSHOT</version>" + "\n" +
+            "           <relativePath>../../../osgi-root/pom.xml</relativePath>" + "\n" +
+            "    </parent>\n" +
+            "</project>\n";
+    }
+}
+
