93a1fdabeb7362a6deb300b700a73ddf711edfe3
Georgii Gvinepadze <fireshowel@gmail.com>
2025-11-12 14:42:59 +0400

    dbeaver/pro#7457 fix NPE
    Signed-off-by: Georgii Gvinepadze <fireshowel@gmail.com>

--- osgi.dependency.processing/src/main/java/com/dbeaver/osgi/dependency/processing/PathsManager.java
+++ osgi.dependency.processing/src/main/java/com/dbeaver/osgi/dependency/processing/PathsManager.java
@@ -60,7 +60,7 @@ public enum PathsManager {
     /**
      * Key - run configuration products uuid, value - list of names of other idea configs to run before launching configuration
      */
-    private Map<String, Set<String>> runBeforeConfigs;
+    private Map<String, Set<String>> runBeforeConfigs = null;
 
     private Map<String, Map<String, String>> propertyValueMap = new LinkedHashMap<>();
     private Map<String, Map<String, String>> envPropertyValueMap = new LinkedHashMap<>();
